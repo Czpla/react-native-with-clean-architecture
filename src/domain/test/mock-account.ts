318444935ea151dd845fc2fbd3b2ed1e9a972aa6
Eduardo Czpla <edu.czpla@gmail.com>
2024-08-12 15:07:06 -0300

    feat: add mockAccount

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,7 +1,12 @@
 import { type Authentication } from '@/domain/usecases/authentication';
+import { type Account } from '@/domain/entities/account-entitie';
 import { faker } from '@faker-js/faker';
 
 export const mockAuthentication = (): Authentication.Params => ({
     email: faker.internet.email(),
     password: faker.internet.password(),
+});
+
+export const mockAccount = (): Account => ({
+    accessToken: faker.string.uuid(),
 });
