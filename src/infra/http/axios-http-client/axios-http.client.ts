3496215ad9ce9ce9332164f48f2d34bfa2d375b2
Eduardo Czpla <edu.czpla@gmail.com>
2024-09-09 17:21:49 -0300

    feat: ensure AxiosHttpClient returns correct values

--- src/infra/http/axios-http-client/axios-http.client.ts
+++ src/infra/http/axios-http-client/axios-http.client.ts
@@ -1,8 +1,14 @@
+import { type HttpResponse } from '@/business/protocols/http';
 import { type HttpPostClient } from '@/business/protocols/http/http-post-client';
 import axios from 'axios';
 
-export class AxiosHttpClient {
-    async post(params: HttpPostClient.Params<any>): Promise<void> {
-        await axios.post(params.url, params.body);
+export class AxiosHttpClient implements HttpPostClient<any, any> {
+    async post(params: HttpPostClient.Params<any>): Promise<HttpResponse<any>> {
+        const httpResponse = await axios.post(params.url, params.body);
+
+        return {
+            statusCode: httpResponse.status,
+            body: httpResponse.data,
+        };
     }
 }
